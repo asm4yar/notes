323e749e243730498fdc3cf67e4d8d46f4e23194
ASM4YAR <asm4yar@ya.ru>
2026-02-12 10:25:18 +0700

    новый файл:    docker/02_monitoring_logging/001_resource_consumption_optimization.sh 	новый файл:    docker/02_monitoring_logging/002_performance_monitoring.sh

---
+++ docker/02_monitoring_logging/002_performance_monitoring.sh
@@ -0,0 +1,19 @@
+# Запуск приложения в контейнере
+docker build -t performance-test .
+docker run -d --name app-container performance-test
+
+# Мониторинг производительности контейнера
+docker stats app-container --no-stream --format "table {{.Container}}\t{{.CPUPerc}}\t{{.MemUsage}}" | tee performance.log
+# выполняем нагрузку ...
+docker stats app-container --no-stream --format "table {{.Container}}\t{{.CPUPerc}}\t{{.MemUsage}}" | tee performance.log
+
+
+# Выборка пиковых значений из логов
+echo "Анализируем данные..."
+grep -Eo '([0-9]+\.[0-9]+)%' performance.log | sort -nr | head -1 | awk '{print "Пиковая загрузка CPU: "$1}'
+grep -Eo '([0-9]+\.[0-9]+MiB)' performance.log | sort -nr | head -1 | awk '{print "Пиковая загрузка памяти: "$1}'
+
+# Завершение работы контейнера после тестирования
+docker stop app-container
+docker rm app-container
+
