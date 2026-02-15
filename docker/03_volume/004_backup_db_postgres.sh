940c39d1db9651fce6a485e8e991b47ca9ce5c77
ASM4YAR <asm4yar@ya.ru>
2026-02-15 18:15:20 +0700

    новый файл:    docker/03_volume/004_backup_db_postgres.sh

---
+++ docker/03_volume/004_backup_db_postgres.sh
@@ -0,0 +1,20 @@
+# Запустите контейнер с PostgreSQL, установив пароль для пользователя
+docker run -d --name postgres_container -e POSTGRES_PASSWORD='MyPasswd' postgres:16
+
+# Подождите, пока PostgreSQL полностью запустится (можно подождать несколько секунд)
+sleep 10
+
+# Создайте базу данных, если она не создана (в данном случае мы уже указали через переменную окружения)
+docker exec -it postgres_container psql -U postgres -c "CREATE DATABASE mydatabase;"
+
+# Создайте таблицу в базе данных для тестирования
+docker exec -it postgres_container psql -U postgres -d mydatabase -c "CREATE TABLE test_table (id SERIAL PRIMARY KEY, name VARCHAR(50));"
+
+# Вставьте тестовые данные в таблицу
+docker exec -it postgres_container psql -U postgres -d mydatabase -c "INSERT INTO test_table (name) VALUES ('Test Entry 1'), ('Test Entry 2');"
+
+# Выполните бэкап базы данных с использованием утилиты pg_dump и сохраните его на хосте
+docker exec -t postgres_container pg_dump -U postgres -d mydatabase > mydatabase_backup.sql
+
+# Проверьте, что бэкап создан
+ls -l mydatabase_backup.sql
